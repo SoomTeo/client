0371ea8d95b509845e59ddf5da446960089ad44a
Donghyun Lee <leedonghyun022@gmail.com>
2025-05-09 22:41:08 +0900

    feat: 공유하기 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from "router2";
 
 import { Home } from "./ui/screen/Home";
 import { Mission } from "./ui/screen/Mission";
+import { Share } from "./ui/screen/Share";
 import { Splash } from "./ui/screen/Splash";
 
 export const App = () => {
@@ -12,6 +13,7 @@ export const App = () => {
           "/": () => <Home />,
           "/404": () => <div>404</div>,
           "/mission": () => <Mission />,
+          "/share": () => <Share />,
         }}
       >
         {(Page) => <Page />}
---
+++ src/ui/screen/Share.tsx
@@ -0,0 +1,10 @@
+import { BottomNavigation } from "../base/BottomNavigation";
+
+export const Share = () => {
+  return (
+    <main>
+      공유하기!
+      <BottomNavigation />
+    </main>
+  );
+};
